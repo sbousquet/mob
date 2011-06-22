1d14bdde878043338ea08226a5de8fcc021392ee
Xavier Jodoin <xjodoin@gmail.com>
2011-06-22 16:12:04 -0400

    correctif pour suppporter eclipse 3.7

--- com.netappsid.mob/test/com/netappsid/mob/ejb3/test/BundleAdapter.java
+++ com.netappsid.mob/test/com/netappsid/mob/ejb3/test/BundleAdapter.java
@@ -3,6 +3,7 @@
  */
 package com.netappsid.mob.ejb3.test;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -26,221 +27,332 @@ import org.osgi.framework.Version;
 public class BundleAdapter implements Bundle
 {
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.Bundle#getState()
 	 */
-	
+
+	@Override
 	public int getState()
 	{
 		return 0;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.Bundle#start(int)
 	 */
-	
+
+	@Override
 	public void start(int options) throws BundleException
 	{}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.Bundle#start()
 	 */
-	
+
+	@Override
 	public void start() throws BundleException
 	{}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.Bundle#stop(int)
 	 */
-	
+
+	@Override
 	public void stop(int options) throws BundleException
 	{}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.Bundle#stop()
 	 */
-	
+
+	@Override
 	public void stop() throws BundleException
 	{}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.Bundle#update(java.io.InputStream)
 	 */
-	
+
+	@Override
 	public void update(InputStream input) throws BundleException
 	{}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.Bundle#update()
 	 */
-	
+
+	@Override
 	public void update() throws BundleException
 	{}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.Bundle#uninstall()
 	 */
-	
+
+	@Override
 	public void uninstall() throws BundleException
 	{}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.Bundle#getHeaders()
 	 */
-	
+
+	@Override
 	public Dictionary getHeaders()
 	{
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.Bundle#getBundleId()
 	 */
-	
+
+	@Override
 	public long getBundleId()
 	{
 		return 0;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.Bundle#getLocation()
 	 */
-	
+
+	@Override
 	public String getLocation()
 	{
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.Bundle#getRegisteredServices()
 	 */
-	
+
+	@Override
 	public ServiceReference[] getRegisteredServices()
 	{
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.Bundle#getServicesInUse()
 	 */
-	
+
+	@Override
 	public ServiceReference[] getServicesInUse()
 	{
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.Bundle#hasPermission(java.lang.Object)
 	 */
-	
+
+	@Override
 	public boolean hasPermission(Object permission)
 	{
 		return false;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.Bundle#getResource(java.lang.String)
 	 */
-	
+
+	@Override
 	public URL getResource(String name)
 	{
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.Bundle#getHeaders(java.lang.String)
 	 */
-	
+
+	@Override
 	public Dictionary getHeaders(String locale)
 	{
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.Bundle#getSymbolicName()
 	 */
-	
+
+	@Override
 	public String getSymbolicName()
 	{
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.Bundle#loadClass(java.lang.String)
 	 */
-	
+
+	@Override
 	public Class loadClass(String name) throws ClassNotFoundException
 	{
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.Bundle#getResources(java.lang.String)
 	 */
-	
+
+	@Override
 	public Enumeration getResources(String name) throws IOException
 	{
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.Bundle#getEntryPaths(java.lang.String)
 	 */
-	
+
+	@Override
 	public Enumeration getEntryPaths(String path)
 	{
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.Bundle#getEntry(java.lang.String)
 	 */
-	
+
+	@Override
 	public URL getEntry(String path)
 	{
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.Bundle#getLastModified()
 	 */
-	
+
+	@Override
 	public long getLastModified()
 	{
 		return 0;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.Bundle#findEntries(java.lang.String, java.lang.String, boolean)
 	 */
-	
+
+	@Override
 	public Enumeration findEntries(String path, String filePattern, boolean recurse)
 	{
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.Bundle#getBundleContext()
 	 */
-	
+
+	@Override
 	public BundleContext getBundleContext()
 	{
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.Bundle#getSignerCertificates(int)
 	 */
-	
+
+	@Override
 	public Map getSignerCertificates(int signersType)
 	{
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.Bundle#getVersion()
 	 */
+	@Override
 	public Version getVersion()
+	{
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Comparable#compareTo(java.lang.Object)
+	 */
+	@Override
+	public int compareTo(Bundle o)
+	{
+		return 0;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.osgi.framework.Bundle#adapt(java.lang.Class)
+	 */
+	@Override
+	public <A> A adapt(Class<A> type)
+	{
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.osgi.framework.Bundle#getDataFile(java.lang.String)
+	 */
+	@Override
+	public File getDataFile(String filename)
 	{
 		return null;
 	}
--- com.netappsid.mob/test/com/netappsid/mob/ejb3/test/BundleContextAdapter.java
+++ com.netappsid.mob/test/com/netappsid/mob/ejb3/test/BundleContextAdapter.java
@@ -5,6 +5,7 @@ package com.netappsid.mob.ejb3.test;
 
 import java.io.File;
 import java.io.InputStream;
+import java.util.Collection;
 import java.util.Dictionary;
 
 import org.osgi.framework.Bundle;
@@ -28,186 +29,294 @@ import org.osgi.framework.ServiceRegistration;
 public class BundleContextAdapter implements BundleContext
 {
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.BundleContext#getProperty(java.lang.String)
 	 */
-	
+
+	@Override
 	public String getProperty(String key)
 	{
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.BundleContext#getBundle()
 	 */
-	
+
+	@Override
 	public Bundle getBundle()
 	{
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.BundleContext#installBundle(java.lang.String, java.io.InputStream)
 	 */
-	
+
+	@Override
 	public Bundle installBundle(String location, InputStream input) throws BundleException
 	{
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.BundleContext#installBundle(java.lang.String)
 	 */
-	
+
+	@Override
 	public Bundle installBundle(String location) throws BundleException
 	{
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.BundleContext#getBundle(long)
 	 */
-	
+
+	@Override
 	public Bundle getBundle(long id)
 	{
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.BundleContext#getBundles()
 	 */
-	
+
+	@Override
 	public Bundle[] getBundles()
 	{
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.BundleContext#addServiceListener(org.osgi.framework.ServiceListener, java.lang.String)
 	 */
-	
+
+	@Override
 	public void addServiceListener(ServiceListener listener, String filter) throws InvalidSyntaxException
 	{}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.BundleContext#addServiceListener(org.osgi.framework.ServiceListener)
 	 */
-	
+
+	@Override
 	public void addServiceListener(ServiceListener listener)
 	{}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.BundleContext#removeServiceListener(org.osgi.framework.ServiceListener)
 	 */
-	
+
+	@Override
 	public void removeServiceListener(ServiceListener listener)
 	{}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.BundleContext#addBundleListener(org.osgi.framework.BundleListener)
 	 */
-	
+
+	@Override
 	public void addBundleListener(BundleListener listener)
 	{}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.BundleContext#removeBundleListener(org.osgi.framework.BundleListener)
 	 */
-	
+
+	@Override
 	public void removeBundleListener(BundleListener listener)
 	{}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.BundleContext#addFrameworkListener(org.osgi.framework.FrameworkListener)
 	 */
-	
+
+	@Override
 	public void addFrameworkListener(FrameworkListener listener)
 	{}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.BundleContext#removeFrameworkListener(org.osgi.framework.FrameworkListener)
 	 */
-	
+
+	@Override
 	public void removeFrameworkListener(FrameworkListener listener)
 	{}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.BundleContext#registerService(java.lang.String[], java.lang.Object, java.util.Dictionary)
 	 */
-	
+
+	@Override
 	public ServiceRegistration registerService(String[] clazzes, Object service, Dictionary properties)
 	{
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.BundleContext#registerService(java.lang.String, java.lang.Object, java.util.Dictionary)
 	 */
-	
+
+	@Override
 	public ServiceRegistration registerService(String clazz, Object service, Dictionary properties)
 	{
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.BundleContext#getServiceReferences(java.lang.String, java.lang.String)
 	 */
-	
+
+	@Override
 	public ServiceReference[] getServiceReferences(String clazz, String filter) throws InvalidSyntaxException
 	{
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.BundleContext#getAllServiceReferences(java.lang.String, java.lang.String)
 	 */
-	
+
+	@Override
 	public ServiceReference[] getAllServiceReferences(String clazz, String filter) throws InvalidSyntaxException
 	{
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.BundleContext#getServiceReference(java.lang.String)
 	 */
-	
+
+	@Override
 	public ServiceReference getServiceReference(String clazz)
 	{
 		return null;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.osgi.framework.BundleContext#getService(org.osgi.framework.ServiceReference)
-	 */
-	
-	public Object getService(ServiceReference reference)
-	{
-		return null;
-	}
-
-	/* (non-Javadoc)
-	 * @see org.osgi.framework.BundleContext#ungetService(org.osgi.framework.ServiceReference)
-	 */
-	
-	public boolean ungetService(ServiceReference reference)
-	{
-		return false;
-	}
-
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.BundleContext#getDataFile(java.lang.String)
 	 */
-	
+
+	@Override
 	public File getDataFile(String filename)
 	{
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.BundleContext#createFilter(java.lang.String)
 	 */
-	
+
+	@Override
 	public Filter createFilter(String filter) throws InvalidSyntaxException
+	{
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.osgi.framework.BundleContext#registerService(java.lang.Class, java.lang.Object, java.util.Dictionary)
+	 */
+	@Override
+	public <S> ServiceRegistration<S> registerService(Class<S> clazz, S service, Dictionary<String, ?> properties)
+	{
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.osgi.framework.BundleContext#getServiceReference(java.lang.Class)
+	 */
+	@Override
+	public <S> ServiceReference<S> getServiceReference(Class<S> clazz)
+	{
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.osgi.framework.BundleContext#getServiceReferences(java.lang.Class, java.lang.String)
+	 */
+	@Override
+	public <S> Collection<ServiceReference<S>> getServiceReferences(Class<S> clazz, String filter) throws InvalidSyntaxException
+	{
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.osgi.framework.BundleContext#getService(org.osgi.framework.ServiceReference)
+	 */
+	@Override
+	public <S> S getService(ServiceReference<S> reference)
+	{
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.osgi.framework.BundleContext#ungetService(org.osgi.framework.ServiceReference)
+	 */
+	@Override
+	public boolean ungetService(ServiceReference<?> reference)
+	{
+		return false;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.osgi.framework.BundleContext#getBundle(java.lang.String)
+	 */
+	@Override
+	public Bundle getBundle(String location)
 	{
 		return null;
 	}
